738effeb3f0a941dca3d717d5c0c47788da8b6e9
warishshahbaz <mdshahbazwarish1996@gmail.com>
2024-05-06 16:36:15 +0530

    implement

--- main.js
+++ main.js
@@ -1,14 +1,40 @@
-const fs = require('fs');
-const readline = require('readline');
+const fs = require("fs");
+const readline = require("readline");
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
-rl.question('Enter the filename: ', (filename) => {
-  rl.question('Enter the word to remove: ', (word) => {
+rl.question("Enter the filename: ", (filename) => {
+  rl.question("Enter the word to remove: ", (word) => {
     // TODO: Implement this function
+    removeWordFromFile(filename, word);
     rl.close();
   });
 });
+
+function removeWordFromFile(filename, wordToRemove) {
+  // Read the file contents
+  fs.readFile(filename, "utf8", (err, data) => {
+    if (err) {
+      console.error(`Error reading file: ${err.message}`);
+      return;
+    }
+
+    // Remove all occurrences of the specified word
+    const modifiedContent = data.replace(
+      new RegExp("\\b" + wordToRemove + "\\b", "g"),
+      ""
+    );
+
+    // Write the modified content back to the file
+    fs.writeFile(filename, modifiedContent, "utf8", (err) => {
+      if (err) {
+        console.error(`Error writing to file: ${err.message}`);
+        return;
+      }
+      console.log(`"${wordToRemove}" removed from ${filename}`);
+    });
+  });
+}
